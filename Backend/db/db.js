9211472572125eab730330d4d3143d357913ea50
Mirza Danish <mirzadanish8109@gmail.com>
2026-07-04 16:00:06 +0530

    Remove .env and move secrets to environment variables

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 async function connectDB(){
   try{
-  await mongoose.connect("mongodb+srv://mirzadanishbaig:Shizuka%40123@businesstracker.8bwvm35.mongodb.net/BusinessTracker");
+  await mongoose.connect(process.env.MONGODB_URI);
   console.log("Database connect Successfully")
 }catch(err){
   console.log("Database connection failed:", err);
